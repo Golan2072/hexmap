5c7b65a8c160ab2703ca31ff588ce58f003361c3
Omer Golan Joel <golan2072@gmail.com>
2021-02-16 00:43:53 +0200

    v0.2

--- hexmap.py
+++ hexmap.py
@@ -1,7 +1,7 @@
 # Hexagon Map Test.
 
-import sys
-from termcolor import colored, cprint
+import os
+import stellagama
 
 
 class World:
@@ -16,21 +16,17 @@ class World:
 # Placeholder Test Sector Data
 
 
-starmap = {1: {1: World("O", "*", "A", "*", "^"), 2: World("@", " ", "C", "*", "^"),
+starmap = {0: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0},
+           1: {1: World("O", "*", "A", "*", "^"), 2: World("@", " ", "C", "*", "^"),
                3: World(" ", " ", " ", " ", " "), 4: World("O", "*", "D", " ", " "),
                5: World("@", " ", "B", " ", " "), 6: World("O", " ", "B", " ", " "),
                7: World("O", " ", "C", " ", " "), 8: World("O", "*", "E", " ", " "),
                9: World("O", " ", "C", " ", " "), 10: World(" ", " ", " ", " ", " ")},
            2: {1: World(" ", " ", " ", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", "@", " ", " ", " "), 4: World("O", "*", "D", " ", " "),
+               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "D", " ", " "),
                5: World("@", " ", "E", " ", " "), 6: World("O", " ", "B", " ", " "),
                7: World("O", " ", "C", " ", " "), 8: World("O", "*", "C", " ", " "),
-               9: World(" ", " ", " ", " ", " "), 10: World("@", " ", "C", " ", " ")},
-           2: {1: World("O", " ", "B", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", "@", " ", " ", " "), 4: World(" ", " ", " ", " ", " "),
-               5: World(" ", " ", " ", " ", " "), 6: World(" ", " ", " ", " ", " "),
-               7: World("O", " ", " ", " ", " "), 8: World("O", "*", "C", " ", " "),
-               9: World("@", "*", "C", " ", " "), 10: World("O", " ", "D", " ", " ")},
+               9: World(" ", " ", " ", " ", " "), 10: World("O", " ", "C", " ", " ")},
            3: {1: World("@", " ", "C", " ", " "), 2: World(" ", " ", " ", " ", " "),
                3: World(" ", " ", " ", " ", " "), 4: World(" ", " ", " ", " ", " "),
                5: World("@", " ", "E", " ", " "), 6: World("O", " ", "B", " ", " "),
@@ -64,24 +60,26 @@ starmap = {1: {1: World("O", "*", "A", "*", "^"), 2: World("@", " ", "C", "*", "
                7: World(" ", " ", " ", " ", " "), 8: World("O", *"D", " ", " ", " "),
                9: World("@", "*", "C", " ", " "), 10: World("O", " ", "D", " ", " "),
                }
-}
+           }
 
 
-def column_check(row, column):
-    if row == 1:
+def hex_number(column, row, world_type):
+    if world_type == " ":
+        return "    "
+    else:
         if column % 2 == 0:
-            return " "
-    else:
-        return column
-
-
-def row_check(row):
-    if row == 0:
-        return " "
-    if row == 10:
-        return 0
-    else:
-        return row
+            row -= 1
+            if row == 0:
+                return "    "
+            if row == 10:
+                return f"0{column}10"
+            else:
+                return f"0{column}0{row}"
+        else:
+            if row == 10:
+                return f"0{column}10"
+            else:
+                return f"0{column}0{row}"
 
 
 def base_row(base_row_string):
@@ -91,14 +89,33 @@ def base_row(base_row_string):
     return row_string
 
 
-cprint(base_row("  ____     "), 'green')
+def even_row(column, row):
+    if column % 2 == 0:
+        if row == 1:
+            return 1
+        else:
+            return row - 1
+    else:
+        return row
 
-for row in range (1, 11):
-    cprint(
-        f" /  {starmap[1][row].starport}{starmap[1][row].gas_giant}\ {column_check(row, 2)} {row_check(row)-1} /  {starmap[3][row].starport}{starmap[3][row].gas_giant}\     /  {starmap[5][row].starport}{starmap[5][row].gas_giant}\     /  {starmap[7][row].starport}{starmap[7][1].gas_giant}\     /", 'green')
-    cprint(
-        f"({starmap[1][row].naval}  {starmap[1][row].worldtype}  )___(   {starmap[3][row].worldtype}  )___(   {starmap[5][row].worldtype}  )___(   {starmap[7][row].worldtype}  )___(", 'green')
-    cprint(
-        f" \{starmap[1][row].scout}1 {row_check(row)}/  {starmap[2][row].starport} {starmap[2][row].gas_giant}\ 3 {row_check(row)}/  {starmap[4][row].starport} {starmap[4][row].gas_giant}\ 5 {row_check(row)}/  {starmap[6][row].starport} {starmap[6][row].gas_giant}\ 7 {row_check(row)}/  {starmap[8][row].starport} {starmap[8][row].gas_giant}\ ", 'green')
-    cprint(
-        f"  ----{starmap[2][row].naval}  {starmap[4][row].worldtype}   ----   {starmap[6][row].worldtype}   ----       ----       )", 'green')
+
+stellagama.clear_screen()
+print(" HEXACORP NEUROCORE OS v.21.1\nDecember 21st 2221 11:32AM")
+print("")
+print(" S T A R M A P  V I E W")
+print("")
+
+print(base_row("   _____      "))
+for row in range(1, 11):
+    print(
+        f"  /  {starmap[1][row].starport} {starmap[1][row].gas_giant}\{starmap[2][even_row(2, row)].scout} {hex_number(2, row, starmap[2][even_row(2, row)].worldtype)} /  {starmap[3][row].starport} {starmap[3][row].gas_giant}\{starmap[4][even_row(4, row)].scout} {hex_number(4, even_row(4, row), starmap[4][even_row(4, row)].worldtype)} /  {starmap[5][row].starport} {starmap[5][row].gas_giant}\{starmap[6][even_row(6, row)].scout} {hex_number(6, row, starmap[6][even_row(6, row)].worldtype)} /  {starmap[7][row].starport} {starmap[7][row].gas_giant}\{starmap[8][even_row(8, row)].scout} {hex_number(8, even_row(8, row), starmap[8][even_row(8, row)].worldtype)} / ")
+    print(
+        f" /{starmap[1][row].naval}  {starmap[1][row].worldtype}   \_____/{starmap[3][row].naval}  {starmap[3][row].worldtype}   \_____/{starmap[5][row].naval}  {starmap[5][row].worldtype}   \_____/{starmap[7][row].naval}  {starmap[7][row].worldtype}   \_____/ ")
+    print(
+        f" \{starmap[1][row].scout} {hex_number(1, row, starmap[1][row].worldtype)} /  {starmap[2][even_row(2, row)].starport} {starmap[2][row].gas_giant}\{starmap[3][row].scout} {hex_number(3, row, starmap[3][row].worldtype)} /  {starmap[4][even_row(4, row)].starport} {starmap[4][row].gas_giant}\{starmap[5][row].scout} {hex_number(5, row, starmap[5][row].worldtype)} /  {starmap[6][even_row(6, row)].starport} {starmap[6][row].gas_giant}\{starmap[7][row].scout} {hex_number(7, row, starmap[7][row].worldtype)} /  {starmap[8][row].starport} {starmap[8][even_row(8, row)].gas_giant}\ ")
+    print(
+        f"  \_____/{starmap[2][even_row(2, row)].naval}  {starmap[2][even_row(2, row)].worldtype}   \_____/{starmap[4][row].naval}  {starmap[4][row].worldtype}   \_____/{starmap[6][row].naval}  {starmap[6][row].worldtype}   \_____/{starmap[8][row].naval}  {starmap[8][row].worldtype}   \ ")
+print(
+    f"        \{starmap[2][10].scout} 0210 /     \{starmap[4][10].scout} 0410 /     \{starmap[6][10].scout} 0610 /     \{starmap[8][10].scout} 0810 /")
+print(f"         \_____/       \_____/       \_____/       \_____/")
+print("")
---
+++ stellagama.py
@@ -0,0 +1,154 @@
+# stellagama.py
+# A module with various useful functions by Omer Golan-Joel
+# v3.1 - July 19th, 2020
+# This is open source code, feel free to use it for any purpose
+# contact me at golan2072@gmail.com
+
+# import modules
+import random
+import os
+import platform
+
+# functions
+
+
+def yn():
+    query = 1
+    while query == 1:
+        answer = input("Y/N: ")
+        if answer.lower() == "y":
+            return "yes"
+            break
+        if answer.lower() == "n":
+            return "no"
+            break
+        else:
+            print("Invalid Answer")
+
+
+def dice(n, sides):
+    die = 0
+    roll = 0
+    while die < n:
+        roll = roll + random.randint(1, sides)
+        die += 1
+    return roll
+
+
+def pseudo_hex(num):
+    num = int(num)
+    code = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q",
+            "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    num = code[num]
+    return num
+
+
+def current_dir():
+    if platform.system() == "Windows":
+        directory = os.listdir(".\\")
+    else:
+        directory = os.getcwd()
+    return directory
+
+
+def check_file_exists(check_file):
+
+    if check_file in os.listdir():
+        file_exists = True
+    else:
+        file_exists = False
+    return file_exists
+
+
+def savefile(extension):
+    filename = str(input("Please enter file name to generate: "))
+    filecheck = filename + "." + extension
+    save = 1
+    if check_file_exists(filecheck):
+        print(" ")
+        print("File already exists. Overwrite?")
+        overwrite = yn()
+        if overwrite == "y":
+            save = 0
+        if overwrite == "n":
+            filename = input("Please enter new file name to generate: ")
+    filename = filename + "." + extension
+    return filename
+
+
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def random_line(filename):
+    with open(filename, "r", encoding='utf-8', errors='ignore') as line_list:
+        line = random.choice(line_list.readlines())
+        line = line.strip()
+    return line
+
+
+class Getch:
+    def __init__(self):
+        try:
+            self.impl = GetchWindows()
+        except ImportError:
+            self.impl = GetchUnix()
+
+    def __call__(self):
+        return self.impl()
+
+
+class GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+def getkeypress():
+    if platform.system() == "Windows":
+        directory = os.listdir(".\\")
+        key = Getch()
+        return key().decode()
+    else:
+        key = Getch()
+        return key()
+
+
+def list_stringer(input_list):
+    output_list = []
+    for item in input_list:
+        output_list.append(str(item))
+    return ' '.join(output_list)
+
+
+def second_highest(number_list):
+    secondary = 0
+    maximal = max(number_list)
+    for number in number_list:
+        if maximal > number > secondary:
+            secondary = number
+        else:
+            number = number
+    return secondary
