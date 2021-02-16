e25a9f13a9fc2d3a97015b11c6eda9dceb1d2ba3
Omer Golan Joel <golan2072@gmail.com>
2021-02-16 20:40:16 +0200

    v0.4

--- hexmap.py
+++ hexmap.py
@@ -12,66 +12,25 @@ class World:
         self.scout = scout
 
 
-# Placeholder Test Sector Data
-
-
-starmap = {0: {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0},
-           1: {0: World(" ", " ", " ", " ", " "), 1: World("O", "*", "A", "*", "^"), 2: World("@", " ", "C", "*", "^"),
-               3: World(" ", " ", " ", " ", " "), 4: World("O", "*", "D", " ", " "),
-               5: World("@", " ", "B", " ", " "), 6: World("O", " ", "B", " ", " "),
-               7: World("O", " ", "C", " ", " "), 8: World("O", "*", "E", " ", " "),
-               9: World("O", " ", "C", " ", " "), 10: World(" ", " ", " ", " ", " ")},
-           2: {0: World(" ", " ", " ", " ", " "),1: World("O", " ", " ", " ", "^"), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "D", " ", " "),
-               5: World("@", " ", "E", " ", " "), 6: World("O", " ", "B", " ", " "),
-               7: World("O", " ", "C", " ", " "), 8: World("O", "*", "C", " ", " "),
-               9: World(" ", " ", " ", " ", " "), 10: World("O", " ", "C", " ", " ")},
-           3: {0: World(" ", " ", " ", " ", " "),1: World("@", " ", "C", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World(" ", " ", " ", " ", " "),
-               5: World("@", " ", "E", " ", " "), 6: World("O", " ", "B", " ", " "),
-               7: World("O", " ", "C", " ", " "), 8: World("O", "*", "C", " ", " "),
-               9: World("@", "*", "C", " ", " "), 10: World("O", " ", "E", " ", " ")},
-           4: {0: World(" ", " ", " ", " ", " "),1: World("@", "*", "D", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "A", "*", "^"),
-               5: World("@", " ", "E", " ", " "), 6: World(" ", " ", " ", " ", " "),
-               7: World("O", " ", "C", " ", " "), 8: World(" ", " ", " ", " ", " "),
-               9: World("O", "*", "E", " ", " "), 10: World("@", " ", "A", "*", " ")},
-           5: {0: World(" ", " ", " ", " ", " "),1: World("@", " ", "E", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "A", "*", "^"),
-               5: World("@", " ", "E", " ", " "), 6: World(" ", " ", " ", " ", " "),
-               7: World("O", " ", "C", " ", " "), 8: World(" ", " ", " ", " ", " "),
-               9: World("O", "*", "E", " ", " "), 10: World("@", " ", "B", "*", " ")},
-           6: {0: World(" ", " ", " ", " ", " "),1: World("@", " ", "A", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "C", "*", "^"),
-               5: World("@", " ", "E", " ", " "), 6: World(" ", " ", " ", " ", " "),
-               7: World(" ", " ", " ", " ", " "), 8: World(" ", " ", " ", " ", " "),
-               9: World("O", "*", "D", " ", " "), 10: World("@", " ", "E", "*", " ")
-               },
-           7: {0: World(" ", " ", " ", " ", " "),1: World(" ", " ", " ", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "D", "*", "^"),
-               5: World(" ", " ", " ", " ", " "), 6: World(" ", " ", " ", " ", " "),
-               7: World(" ", " ", " ", " ", " "), 8: World(" ", " ", " ", " ", " "),
-               9: World("O", "*", "E", " ", " "), 10: World(" ", " ", " ", " ", " ")
-               },
-           8: {0: World(" ", " ", " ", " ", " "),1: World(" ", " ", " ", " ", " "), 2: World(" ", " ", " ", " ", " "),
-               3: World(" ", " ", " ", " ", " "), 4: World("@", "*", "C", "*", "^"),
-               5: World(" ", " ", " ", " ", " "), 6: World(" ", " ", " ", " ", " "),
-               7: World(" ", " ", " ", " ", " "), 8: World("O", *"D", " ", " ", " "),
-               9: World("@", "*", "C", " ", " "), 10: World("O", " ", "D", " ", " "),
-               }
-           }
+def blank_map():
+    starmap = {}
+    for column in range(0, 9):
+        starmap[column] = {}
+        for row in range(0, 11):
+            starmap[column][row] = World(" ", " ", " ", " ", " ")
+    return starmap
 
 
 def hex_number(column, row, worldtype):
     if column % 2 == 0:
-        if row == 1 or 0:
+        if (row == 1) or (row == 0):
             return "    "
         elif worldtype == " ":
             return "    "
         elif row == 11:
             return f"0{column}10"
         else:
-            return f"0{column}0{row-1}"
+            return f"0{column}0{row - 1}"
     else:
         if worldtype == " ":
             return "    "
@@ -98,27 +57,22 @@ def even_row(column, row):
         return row
 
 
-def starmap_print():
+def starmap_string(starmap):
+    global row
     stellagama.clear_screen()
-    print(" HEXACORP NEUROCORE OS v.21.1\n December 21st 2221 11:32AM")
-    print("")
-    print(" S T A R M A P  V I E W")
-    print("")
+    star_string = f"HEXACORP NEUROCORE OS v.21.1\n December 21st 2221 11:32AM\n\nS T A R M A P  V I E W\n\n {base_row('  _____       ')}\n"
 
-    print(base_row("   _____      "))
     for row in range(1, 11):
-        print(
-            f"  /  {starmap[1][row].starport} {starmap[1][row].gas_giant}\{starmap[2][row-1].scout} {hex_number(2, row, starmap[2][row-1].worldtype)} /  {starmap[3][row].starport} {starmap[3][row].gas_giant}\{starmap[4][row-1].scout} {hex_number(4, row, starmap[4][row-1].worldtype)} /  {starmap[5][row].starport} {starmap[5][row].gas_giant}\{starmap[6][row-1].scout} {hex_number(6, row, starmap[6][row-1].worldtype)} /  {starmap[7][row].starport} {starmap[7][row].gas_giant}\{starmap[8][row-1].scout} {hex_number(8, row, starmap[8][row-1].worldtype)} / ")
-        print(
-            f" /{starmap[1][row].naval}  {starmap[1][row].worldtype}   \_____/{starmap[3][row].naval}  {starmap[3][row].worldtype}   \_____/{starmap[5][row].naval}  {starmap[5][row].worldtype}   \_____/{starmap[7][row].naval}  {starmap[7][row].worldtype}   \_____/ ")
-        print(
-            f" \{starmap[1][row].scout} {hex_number(1, row, starmap[1][row].worldtype)} /  {starmap[2][row].starport} {starmap[2][row].gas_giant}\{starmap[3][row].scout} {hex_number(3, row, starmap[3][row].worldtype)} /  {starmap[4][row].starport} {starmap[4][row].gas_giant}\{starmap[5][row].scout} {hex_number(5, row, starmap[5][row].worldtype)} /  {starmap[6][row].starport} {starmap[6][row].gas_giant}\{starmap[7][row].scout} {hex_number(7, row, starmap[7][row].worldtype)} /  {starmap[8][row].starport} {starmap[8][row].gas_giant}\ ")
-        print(
-            f"  \_____/{starmap[2][row].naval}  {starmap[2][row].worldtype}   \_____/{starmap[4][row].naval}  {starmap[4][row].worldtype}   \_____/{starmap[6][row].naval}  {starmap[6][row].worldtype}   \_____/{starmap[8][row].naval}  {starmap[8][row].worldtype}   \ ")
-    print(
-        f"        \{starmap[2][10].scout} 0210 /     \{starmap[4][10].scout} 0410 /     \{starmap[6][10].scout} 0610 /     \{starmap[8][10].scout} 0810 /")
-    print(f"         \_____/       \_____/       \_____/       \_____/")
-    print("")
+        star_string += f"  /  {starmap[1][row].starport} {starmap[1][row].gas_giant}\{starmap[2][row - 1].scout} {hex_number(2, row, starmap[2][row - 1].worldtype)} /  {starmap[3][row].starport} {starmap[3][row].gas_giant}\{starmap[4][row - 1].scout} {hex_number(4, row, starmap[4][row - 1].worldtype)} /  {starmap[5][row].starport} {starmap[5][row].gas_giant}\{starmap[6][row - 1].scout} {hex_number(6, row, starmap[6][row - 1].worldtype)} /  {starmap[7][row].starport} {starmap[7][row].gas_giant}\{starmap[8][row - 1].scout} {hex_number(8, row, starmap[8][row - 1].worldtype)} / \n"
+        star_string += f" /{starmap[1][row].naval}  {starmap[1][row].worldtype}   \_____/{starmap[3][row].naval}  {starmap[3][row].worldtype}   \_____/{starmap[5][row].naval}  {starmap[5][row].worldtype}   \_____/{starmap[7][row].naval}  {starmap[7][row].worldtype}   \_____/ \n"
+        star_string += f" \{starmap[1][row].scout} {hex_number(1, row, starmap[1][row].worldtype)} /  {starmap[2][row].starport} {starmap[2][row].gas_giant}\{starmap[3][row].scout} {hex_number(3, row, starmap[3][row].worldtype)} /  {starmap[4][row].starport} {starmap[4][row].gas_giant}\{starmap[5][row].scout} {hex_number(5, row, starmap[5][row].worldtype)} /  {starmap[6][row].starport} {starmap[6][row].gas_giant}\{starmap[7][row].scout} {hex_number(7, row, starmap[7][row].worldtype)} /  {starmap[8][row].starport} {starmap[8][row].gas_giant}\ \n"
+        star_string += f"  \_____/{starmap[2][row].naval}  {starmap[2][row].worldtype}   \_____/{starmap[4][row].naval}  {starmap[4][row].worldtype}   \_____/{starmap[6][row].naval}  {starmap[6][row].worldtype}   \_____/{starmap[8][row].naval}  {starmap[8][row].worldtype}   \ \n"
+    star_string += f"        \{starmap[2][10].scout} {hex_number(2, row, starmap[2][row - 1].worldtype)} /     \{starmap[4][10].scout} {hex_number(4, row, starmap[4][row - 1].worldtype)} /     \{starmap[6][10].scout} {hex_number(6, row, starmap[6][row - 1].worldtype)} /     \{starmap[8][10].scout} {hex_number(8, row, starmap[8][row - 1].worldtype)} /\n"
+    star_string += f"         \_____/       \_____/       \_____/       \_____/\n\n"
+    return star_string
+
 
 if __name__ == "__main__":
-    starmap_print()
+    starmap = blank_map()
+    starmap[1][2] = World("@", "*", "A", "*", "^")
+    print(starmap_string(starmap))
